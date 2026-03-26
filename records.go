7644e187c4034cd219fed877e4cc9f22010da4f1
Seth Hoenig <shoenig@duck.com>
2026-03-26 13:03:34 -0500

    records: update comment for id type

--- records.go
+++ records.go
@@ -6,4 +6,10 @@ const (
 	ExecFailure = -1
 )
 
+// ID represents a unique ROW ID of a table.
+//
+// Most often, this is coming from an autoincrement index.
+//
+// A common pattern is to create a new type based on this type, so as to
+// leverage the type system ensuring ID values are not co-mingled.
 type ID int
